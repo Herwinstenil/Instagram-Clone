9d30d72e0d9e973cb65efd82fe095c8b1c8a54bf
Herwinstenil <herwinstenil24@gmail.com>
2025-10-04 20:26:57 +0530

    Instagram

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <div>Sidebar</div>
+      <div>Feed</div>
+      <div>Suggestions</div>
+    </div>
   )
 }
 
