da6576886abaaaba14a76bcaa42e4cdecb7dd58d
Herwinstenil <herwinstenil24@gmail.com>
2025-10-05 19:20:42 +0530

    Instagram

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <div className='d-flex vh-100'>
-      <div className='w-20'>Sidebar</div>
+      <div className='w-20'><Sidebar/></div>
       <div className='w-50 bg-info'>Feed</div>
       <div className='w-30'>Suggestions</div>
     </div>
