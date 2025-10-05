677973cc72ccfb2755339b6d715c523570f3e774
Herwinstenil <herwinstenil24@gmail.com>
2025-10-05 19:35:00 +0530

    Instagram

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 function Sidebar() {
   return (
+    <>
     <div>
       <img className='logo-text' src="src\assets\Instagram_text.png" alt="" />
+      <div>Home</div>
+      <div>Search</div>
+      <div>Explore</div>
+      <div>Reels</div>
+      <div>Messages</div>
+      <div>Notifications</div>
+      <div>Create</div>
+      <div>Profile</div>
     </div>
+    <div>
+      <div>Threads</div>
+      <div>More</div>
+    </div>
+    </>
   )
 }
 
