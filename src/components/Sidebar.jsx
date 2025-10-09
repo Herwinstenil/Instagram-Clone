56eec7e7b829cc726e4cbe27aeb23d3c77a3fb1c
Herwinstenil <herwinstenil24@gmail.com>
2025-10-09 12:24:03 +0530

    Instagram

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,12 +5,12 @@ function Sidebar() {
     <>
     <div>
       <img className='logo-text' src="src\assets\Instagram_text.png" alt="" />
-      <div>Home</div>
-      <div>Search</div>
-      <div>Explore</div>
-      <div>Reels</div>
-      <div>Messages</div>
-      <div>Notifications</div>
+      <div><i class="bi bi-house-door-fill"></i>Home</div>
+      <div><i class="bi bi-search"></i>Search</div>
+      <div><i class="bi bi-compass"></i>Explore</div>
+      <div><i class="bi bi-play-circle"></i>Reels</div>
+      <div><i class="bi bi-chat-dots"></i>Messages</div>
+      <div><i class="bi bi-heart"></i>Notifications</div>
       <div>Create</div>
       <div>Profile</div>
     </div>
