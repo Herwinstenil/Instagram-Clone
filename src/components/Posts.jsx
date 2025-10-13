1347233f47ddb3f3213c4c629ad1b8eb36aeb378
Herwinstenil <herwinstenil24@gmail.com>
2025-10-13 16:11:50 +0530

    Instagram

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
 function Posts() {
+
+  const[posts, setPosts] = useState([]);
+  
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => setPosts(data))
+      .catch(error => console.error('Error fetching posts:', error));
+  }, []);
+
   return (
     <div>Posts</div>
   )
