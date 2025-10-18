29f00d0f5d152d60f11df3f4d8b96a594c1d45fe
Herwinstenil <herwinstenil24@gmail.com>
2025-10-18 19:27:49 +0530

    Instagram

--- src/components/Suggestions.jsx
+++ src/components/Suggestions.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function Suggestions() {
   return (
     <div>
-      
+      <div className='d-flex'>
+        <img className='dp rounded-circle' src={post.user.profile_pic} alt="Profile pic" />
+        <h5>{post.user.username}</h5>
+      </div>
     </div>
   )
 }
