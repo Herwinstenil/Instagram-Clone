365dbb967e6cd8ef77db8a7ea50ed368292bc281
Herwinstenil <herwinstenil24@gmail.com>
2025-10-19 23:14:21 +0530

    Instagram

--- src/components/Suggestions.jsx
+++ src/components/Suggestions.jsx
@@ -8,27 +8,29 @@ function Suggestions() {
   useEffect(() => {
 
     fetch('http://localhost:3000/profile')
-    .then(data => data.json())
-    .then(data => setProfile(data))
-    .catch(err => console.log('Error fetching profile:', err))
+      .then(data => data.json())
+      .then(data => setProfile(data))
+      .catch(err => console.log('Error fetching profile:', err))
 
     fetch('http://localhost:3000/suggestions')
-    .then(data => data.json())
-    .then(data => setSuggestions(data))
-    .catch(err => console.log('Error fetching suggestions:', err))
+      .then(data => data.json())
+      .then(data => setSuggestions(data))
+      .catch(err => console.log('Error fetching suggestions:', err))
 
-  }, []);  
+  }, []);
 
   return (
     <div>
-      {profile ?
-      <div className='d-flex'>
-        <img className='dp rounded-circle' src={profile.profile_pic} alt="Profile pic" />
-        <h5>{profile.username}</h5>
-        <p>Switch</p>
+      <div className='suggestions'>
+        {profile ?
+          <div className='d-flex'>
+            <img className='dp rounded-circle' src={profile.profile_pic} alt="Profile pic" />
+            <h5>{profile.username}</h5>
+            <p className='ms-auto'>Switch</p>
+          </div>
+          : <p>Loading Profile...</p>
+        }
       </div>
-      : <p>Loading Profile...</p>
-      }
     </div>
   )
 }
