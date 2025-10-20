430a95018e77b931cb653ab4a332317c67cb92af
Herwinstenil <herwinstenil24@gmail.com>
2025-10-20 20:32:15 +0530

    Instagram

--- src/components/Suggestions.jsx
+++ src/components/Suggestions.jsx
@@ -36,6 +36,33 @@ function Suggestions() {
           <b className='ms-auto'>See All</b>
         </div>
 
+         {posts.length > 0 ? (
+            <div>
+              {posts.map((post) => (
+                <div className='my-3' key={post.id}>
+                  <div className='d-flex'>
+                    <img className='dp rounded-circle' src={post.user.profile_pic} alt="Profile pic" />
+                    <h5>{post.user.username}</h5>
+                  </div>
+                  <img className='image' src={post.image} alt="Post image" />
+                  <div>
+                    <i className="bi bi-heart"></i>
+                    <i className="bi bi-chat"></i>
+                    <i className="bi bi-send"></i>
+                  </div>
+                  <div>
+                    <b>{post.likes} Likes</b>
+                  </div>
+                  <p>{post.caption}</p>
+                </div>
+              ))}
+            </div>
+        ) : (
+            <div>
+              Loading posts...
+            </div>
+        )}
+        
       </div>
     </div>
   )
