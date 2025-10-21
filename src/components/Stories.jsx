8da9e7d14a16645427656a968a4fcaa60fa3abdf
Herwinstenil <herwinstenil24@gmail.com>
2025-10-21 21:19:05 +0530

    Instagram

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,10 +1,16 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Stories() {
 
   const [stories, setStories] = useState([]);
 
-  
+  useEffect(() => {
+    fetch('http://localhost:3000/story')
+      .then(data => data.json())
+      .then(data => setStories(data))
+      .catch(err => console.log('Error fetching stories:', err));
+  }, []);
+
   return (
     <div className='story'>
 
