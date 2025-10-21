ec419586919b5bbcaadffa9e28ab8276b455542a
Herwinstenil <herwinstenil24@gmail.com>
2025-10-21 21:13:03 +0530

    Instagram

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 
 function Stories() {
+
+  const [stories, setStories] = useState([]);
+
+  
   return (
     <div className='story'>
-      
+
     </div>
   )
 }
