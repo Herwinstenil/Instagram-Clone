a413f08ce5dace8146fa07ae5d0217c3a826da9e
Herwinstenil <herwinstenil24@gmail.com>
2025-10-24 20:50:49 +0530

    Instagram

--- src/components/ViewStory.jsx
+++ src/components/ViewStory.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
 
 function viewstory() {
-  return (
-    <div>viewstory</div>
-  )
+
+    const { id } = useParams();
+
+    const [story, setStory] = useState(null);
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/story/${id}`)
+            .then(data => data.json())
+            .then(data => setStory(data))
+            .catch(err => console.log('Error fetching story:', err));
+    }, []);
+
+    return (
+        <div>
+            {story? <div>story.user.username</div> : <div>Loading...</div>}
+        </div>
+    )
 }
 
 export default viewstory
