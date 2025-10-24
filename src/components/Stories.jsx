6bf5586be7d415bddbe0c59c00e7711feaef3a56
Herwinstenil <herwinstenil24@gmail.com>
2025-10-24 20:00:25 +0530

    Instagram

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -15,9 +15,11 @@ function Stories() {
     <div className='story d-flex'>
       {stories.length > 0 ? (
         stories.map((story) => (
-          <div key={story.id}> 
-            <img className='story-dp rounded-circle' src={story.user.profile_pic} alt="Story pic" />
-            <p className='text-truncate' style={{width:"50px"}}>{ story.user.username }</p>
+          <div key={story.id}>
+            <div className='gradient-border '>
+              <img className='story-dp rounded-circle' src={story.user.profile_pic} alt="Story pic" />
+            </div>
+            <p className='text-truncate' style={{ width: "50px" }}>{story.user.username}</p>
           </div>
         ))
       ) : (
