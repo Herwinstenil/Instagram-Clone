1f382f36140eb24a9981e7d821ada5805b9c3c5c
Herwinstenil <herwinstenil24@gmail.com>
2025-10-29 17:41:05 +0530

    Instagram

--- src/components/ViewStory.jsx
+++ src/components/ViewStory.jsx
@@ -23,9 +23,9 @@ function ViewStory() {
     return (
         <div>
             {story ? <div className='d-flex justify-content-center align-items-center'>
-                <Link to ={`http://localhost:5173/story/${Number(id)-1}/${tot}`}><i class="bi bi-arrow-left-circle-fill"></i></Link>
+                <Link to ={`/story/${Number(id)-1}/${tot}`}><i class="bi bi-arrow-left-circle-fill"></i></Link>
                 <img className='vh-100' src={story.image} alt="story" />
-                <Link to ={`http://localhost:5173/story/${Number(id)+1}/${tot}`}><i class="bi bi-arrow-right-circle-fill"></i></Link>
+                <Link to ={`/story/${Number(id)+1}/${tot}`}><i class="bi bi-arrow-right-circle-fill"></i></Link>
             </div> :
 
                 <div>Loading...</div>}
