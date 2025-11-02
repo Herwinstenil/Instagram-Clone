40e5d7f49614e687a503494cf05058bcc9a3dd80
Herwinstenil <herwinstenil24@gmail.com>
2025-11-02 17:06:25 +0530

    Instagram

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -11,6 +11,13 @@ function Profile() {
             .then(data => { setProfile(data.data); console.log(data.data) })
     }, [])
 
+    function HandleOnChange(e) {
+        setProfile(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    }
+
     return (
         <div className='m-5'>
             {profile ? (
@@ -22,17 +29,19 @@ function Profile() {
                         value={profile.username}
                         name='username'
                         className='form-control my-4'
+                        onChange={HandleOnChange}
                     />
 
                     <input type="text"
                         value={profile.profile_pic}
                         name='profile_pic'
                         className='form-control'
+                        onChange={HandleOnChange}
                     />
                     <button className='btn btn-primary mt-3'>
                         Update Profile
                     </button>
-                    
+
                 </div>
             ) : (
                 <div>Loading Profile...</div>
