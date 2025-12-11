9e1d795ea990f8448c7b62cc0a059bf8b835dfad
Herwinstenil <herwinstenil24@gmail.com>
2025-12-11 16:34:09 +0530

    Instagram

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import jsonServer from "json-server";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
